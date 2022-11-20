ffbb66786d71d628ee6b62576eb5832d384ebe55
Martin Hloska <martin.hloska@gmail.com>
2022-11-20 09:57:15 +0100

    reformat

--- pallet-amm/src/lib.rs
+++ pallet-amm/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-
 use frame_support::{dispatch::DispatchResult, traits::Get};
 use frame_system::ensure_signed;
 
@@ -15,98 +14,96 @@ type Balance = u128;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::OriginFor;
+    use super::*;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::OriginFor;
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+    #[pallet::hooks]
+    impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
         type WeightInfo: WeightInfo;
-	}
+    }
 
-	#[pallet::error]
-	pub enum Error<T> {
-	}
+    #[pallet::error]
+    pub enum Error<T> {}
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
-	pub enum Event<T: Config> {
-	}
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(crate) fn deposit_event)]
+    pub enum Event<T: Config> {}
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		#[pallet::weight(<T as Config>::WeightInfo::create_pool())]
-		pub fn create_pool(
-			origin: OriginFor<T>,
-			_asset_a: AssetId,
-			_amount_a: Balance,
-			_asset_b: AssetId,
-			_amount_b: Balance,
-		) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::weight(<T as Config>::WeightInfo::create_pool())]
+        pub fn create_pool(
+            origin: OriginFor<T>,
+            _asset_a: AssetId,
+            _amount_a: Balance,
+            _asset_b: AssetId,
+            _amount_b: Balance,
+        ) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		#[pallet::weight(<T as Config>::WeightInfo::add_liquidity())]
-		pub fn add_liquidity(
-			origin: OriginFor<T>,
-			_asset_a: AssetId,
-			_asset_b: AssetId,
-			_amount_a: Balance,
-			_amount_b_max_limit: Balance,
-		) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+        #[pallet::weight(<T as Config>::WeightInfo::add_liquidity())]
+        pub fn add_liquidity(
+            origin: OriginFor<T>,
+            _asset_a: AssetId,
+            _asset_b: AssetId,
+            _amount_a: Balance,
+            _amount_b_max_limit: Balance,
+        ) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		#[pallet::weight(<T as Config>::WeightInfo::remove_liquidity())]
-		pub fn remove_liquidity(
-			origin: OriginFor<T>,
-			_asset_a: AssetId,
-			_asset_b: AssetId,
-			_liquidity_amount: Balance,
-		) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+        #[pallet::weight(<T as Config>::WeightInfo::remove_liquidity())]
+        pub fn remove_liquidity(
+            origin: OriginFor<T>,
+            _asset_a: AssetId,
+            _asset_b: AssetId,
+            _liquidity_amount: Balance,
+        ) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		#[pallet::weight(<T as Config>::WeightInfo::sell())]
-		pub fn sell(
-			origin: OriginFor<T>,
-			_asset_in: AssetId,
-			_asset_out: AssetId,
-			_amount: Balance,
-			_max_limit: Balance,
-			_discount: bool,
-		) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+        #[pallet::weight(<T as Config>::WeightInfo::sell())]
+        pub fn sell(
+            origin: OriginFor<T>,
+            _asset_in: AssetId,
+            _asset_out: AssetId,
+            _amount: Balance,
+            _max_limit: Balance,
+            _discount: bool,
+        ) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		#[pallet::weight(<T as Config>::WeightInfo::buy())]
-		pub fn buy(
-			origin: OriginFor<T>,
-			_asset_out: AssetId,
-			_asset_in: AssetId,
-			_amount: Balance,
-			_max_limit: Balance,
-			_discount: bool,
-		) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+        #[pallet::weight(<T as Config>::WeightInfo::buy())]
+        pub fn buy(
+            origin: OriginFor<T>,
+            _asset_out: AssetId,
+            _asset_in: AssetId,
+            _amount: Balance,
+            _max_limit: Balance,
+            _discount: bool,
+        ) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
 
-			Ok(())
-		}
-	}
+            Ok(())
+        }
+    }
 }
--- pallet-amm/src/weights.rs
+++ pallet-amm/src/weights.rs
@@ -40,76 +40,76 @@
 #![allow(clippy::unnecessary_cast)]
 
 use frame_support::{
-	traits::Get,
-	weights::{constants::RocksDbWeight, Weight},
+    traits::Get,
+    weights::{constants::RocksDbWeight, Weight},
 };
 use sp_std::marker::PhantomData;
 
 /// Weight functions needed for amm.
 pub trait WeightInfo {
-	fn create_pool() -> Weight;
-	fn add_liquidity() -> Weight;
-	fn remove_liquidity() -> Weight;
-	fn sell() -> Weight;
-	fn buy() -> Weight;
+    fn create_pool() -> Weight;
+    fn add_liquidity() -> Weight;
+    fn remove_liquidity() -> Weight;
+    fn sell() -> Weight;
+    fn buy() -> Weight;
 }
 
 /// Weights for amm using the hydraDX node and recommended hardware.
 pub struct HydraWeight<T>(PhantomData<T>);
 
 impl<T: frame_system::Config> WeightInfo for HydraWeight<T> {
-	fn create_pool() -> Weight {
-		Weight::from_ref_time(189_645_000 as u64)
-			.saturating_add(T::DbWeight::get().reads(11 as u64))
-			.saturating_add(T::DbWeight::get().writes(13 as u64))
-	}
-	fn add_liquidity() -> Weight {
-		Weight::from_ref_time(171_602_000 as u64)
-			.saturating_add(T::DbWeight::get().reads(9 as u64))
-			.saturating_add(T::DbWeight::get().writes(8 as u64))
-	}
-	fn remove_liquidity() -> Weight {
-		Weight::from_ref_time(170_846_000 as u64)
-			.saturating_add(T::DbWeight::get().reads(8 as u64))
-			.saturating_add(T::DbWeight::get().writes(7 as u64))
-	}
-	fn sell() -> Weight {
-		Weight::from_ref_time(122_125_000 as u64)
-			.saturating_add(T::DbWeight::get().reads(5 as u64))
-			.saturating_add(T::DbWeight::get().writes(4 as u64))
-	}
-	fn buy() -> Weight {
-		Weight::from_ref_time(121_289_000 as u64)
-			.saturating_add(T::DbWeight::get().reads(5 as u64))
-			.saturating_add(T::DbWeight::get().writes(4 as u64))
-	}
+    fn create_pool() -> Weight {
+        Weight::from_ref_time(189_645_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(11 as u64))
+            .saturating_add(T::DbWeight::get().writes(13 as u64))
+    }
+    fn add_liquidity() -> Weight {
+        Weight::from_ref_time(171_602_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(9 as u64))
+            .saturating_add(T::DbWeight::get().writes(8 as u64))
+    }
+    fn remove_liquidity() -> Weight {
+        Weight::from_ref_time(170_846_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(8 as u64))
+            .saturating_add(T::DbWeight::get().writes(7 as u64))
+    }
+    fn sell() -> Weight {
+        Weight::from_ref_time(122_125_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(5 as u64))
+            .saturating_add(T::DbWeight::get().writes(4 as u64))
+    }
+    fn buy() -> Weight {
+        Weight::from_ref_time(121_289_000 as u64)
+            .saturating_add(T::DbWeight::get().reads(5 as u64))
+            .saturating_add(T::DbWeight::get().writes(4 as u64))
+    }
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
-	fn create_pool() -> Weight {
-		Weight::from_ref_time(189_645_000 as u64)
-			.saturating_add(RocksDbWeight::get().reads(11 as u64))
-			.saturating_add(RocksDbWeight::get().writes(13 as u64))
-	}
-	fn add_liquidity() -> Weight {
-		Weight::from_ref_time(171_602_000 as u64)
-			.saturating_add(RocksDbWeight::get().reads(9 as u64))
-			.saturating_add(RocksDbWeight::get().writes(8 as u64))
-	}
-	fn remove_liquidity() -> Weight {
-		Weight::from_ref_time(170_846_000 as u64)
-			.saturating_add(RocksDbWeight::get().reads(8 as u64))
-			.saturating_add(RocksDbWeight::get().writes(7 as u64))
-	}
-	fn sell() -> Weight {
-		Weight::from_ref_time(122_125_000 as u64)
-			.saturating_add(RocksDbWeight::get().reads(5 as u64))
-			.saturating_add(RocksDbWeight::get().writes(4 as u64))
-	}
-	fn buy() -> Weight {
-		Weight::from_ref_time(121_289_000 as u64)
-			.saturating_add(RocksDbWeight::get().reads(5 as u64))
-			.saturating_add(RocksDbWeight::get().writes(4 as u64))
-	}
+    fn create_pool() -> Weight {
+        Weight::from_ref_time(189_645_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(11 as u64))
+            .saturating_add(RocksDbWeight::get().writes(13 as u64))
+    }
+    fn add_liquidity() -> Weight {
+        Weight::from_ref_time(171_602_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(9 as u64))
+            .saturating_add(RocksDbWeight::get().writes(8 as u64))
+    }
+    fn remove_liquidity() -> Weight {
+        Weight::from_ref_time(170_846_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(8 as u64))
+            .saturating_add(RocksDbWeight::get().writes(7 as u64))
+    }
+    fn sell() -> Weight {
+        Weight::from_ref_time(122_125_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(5 as u64))
+            .saturating_add(RocksDbWeight::get().writes(4 as u64))
+    }
+    fn buy() -> Weight {
+        Weight::from_ref_time(121_289_000 as u64)
+            .saturating_add(RocksDbWeight::get().reads(5 as u64))
+            .saturating_add(RocksDbWeight::get().writes(4 as u64))
+    }
 }
